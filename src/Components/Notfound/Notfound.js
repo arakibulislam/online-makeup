f035430204b94b6581f60937912bc4da469c532a
maisha haider <arakibulislam>
2021-10-04 20:25:21 +0600

    final

--- src/Components/Notfound/Notfound.js
+++ src/Components/Notfound/Notfound.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Alert } from 'react-bootstrap';
+import error from '../../images/error.jpg'
 
 const Notfound = () => {
     return (
-        <div className='mt-5 pt-5 text-center w-50 mx-auto'>
-            <Alert variant="danger">
-                404 Page Not Found !
-            </Alert>
+        <div>
+            <img className='w-100' src={error} alt="" />
         </div>
     );
 };
